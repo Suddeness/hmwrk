193eafed7603c3b31309c6135c3467b65303c7d2
Suddeness <lezer194@gmail.com>
2025-03-03 12:18:56 +0100

    Create designer_example_for_all_v.cs

---
+++ lb13/designer_example_for_all_v.cs
@@ -0,0 +1,210 @@
+namespace Draw2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Radio_Box = new System.Windows.Forms.GroupBox();
+            this.NR3 = new System.Windows.Forms.RadioButton();
+            this.NR2 = new System.Windows.Forms.RadioButton();
+            this.NR1 = new System.Windows.Forms.RadioButton();
+            this.Check_Box = new System.Windows.Forms.GroupBox();
+            this.worker4 = new System.Windows.Forms.CheckBox();
+            this.worker5 = new System.Windows.Forms.CheckBox();
+            this.worker3 = new System.Windows.Forms.CheckBox();
+            this.worker2 = new System.Windows.Forms.CheckBox();
+            this.Chicken = new System.Windows.Forms.CheckBox();
+            this.btn_cnfrm = new System.Windows.Forms.Button();
+            this.txt_bx = new System.Windows.Forms.TextBox();
+            this.Radio_Box.SuspendLayout();
+            this.Check_Box.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Radio_Box
+            // 
+            this.Radio_Box.Controls.Add(this.NR3);
+            this.Radio_Box.Controls.Add(this.NR2);
+            this.Radio_Box.Controls.Add(this.NR1);
+            this.Radio_Box.Location = new System.Drawing.Point(385, 24);
+            this.Radio_Box.Name = "Radio_Box";
+            this.Radio_Box.Size = new System.Drawing.Size(277, 342);
+            this.Radio_Box.TabIndex = 0;
+            this.Radio_Box.TabStop = false;
+            this.Radio_Box.Text = "groupBox1";
+            // 
+            // NR3
+            // 
+            this.NR3.AutoSize = true;
+            this.NR3.Location = new System.Drawing.Point(31, 243);
+            this.NR3.Name = "NR3";
+            this.NR3.Size = new System.Drawing.Size(47, 17);
+            this.NR3.TabIndex = 2;
+            this.NR3.TabStop = true;
+            this.NR3.Text = "NR3";
+            this.NR3.UseVisualStyleBackColor = true;
+            // 
+            // NR2
+            // 
+            this.NR2.AutoSize = true;
+            this.NR2.Location = new System.Drawing.Point(31, 137);
+            this.NR2.Name = "NR2";
+            this.NR2.Size = new System.Drawing.Size(47, 17);
+            this.NR2.TabIndex = 1;
+            this.NR2.TabStop = true;
+            this.NR2.Text = "NR2";
+            this.NR2.UseVisualStyleBackColor = true;
+            // 
+            // NR1
+            // 
+            this.NR1.AutoSize = true;
+            this.NR1.Location = new System.Drawing.Point(31, 45);
+            this.NR1.Name = "NR1";
+            this.NR1.Size = new System.Drawing.Size(47, 17);
+            this.NR1.TabIndex = 0;
+            this.NR1.TabStop = true;
+            this.NR1.Text = "NR1";
+            this.NR1.UseVisualStyleBackColor = true;
+            // 
+            // Check_Box
+            // 
+            this.Check_Box.Controls.Add(this.worker4);
+            this.Check_Box.Controls.Add(this.worker5);
+            this.Check_Box.Controls.Add(this.worker3);
+            this.Check_Box.Controls.Add(this.worker2);
+            this.Check_Box.Controls.Add(this.Chicken);
+            this.Check_Box.Location = new System.Drawing.Point(21, 24);
+            this.Check_Box.Name = "Check_Box";
+            this.Check_Box.Size = new System.Drawing.Size(277, 342);
+            this.Check_Box.TabIndex = 1;
+            this.Check_Box.TabStop = false;
+            this.Check_Box.Text = "groupBox2";
+            // 
+            // worker4
+            // 
+            this.worker4.AutoSize = true;
+            this.worker4.Location = new System.Drawing.Point(22, 191);
+            this.worker4.Name = "worker4";
+            this.worker4.Size = new System.Drawing.Size(64, 17);
+            this.worker4.TabIndex = 4;
+            this.worker4.Text = "worker4";
+            this.worker4.UseVisualStyleBackColor = true;
+            // 
+            // worker5
+            // 
+            this.worker5.AutoSize = true;
+            this.worker5.Location = new System.Drawing.Point(22, 243);
+            this.worker5.Name = "worker5";
+            this.worker5.Size = new System.Drawing.Size(64, 17);
+            this.worker5.TabIndex = 3;
+            this.worker5.Text = "worker5";
+            this.worker5.UseVisualStyleBackColor = true;
+            // 
+            // worker3
+            // 
+            this.worker3.AutoSize = true;
+            this.worker3.Location = new System.Drawing.Point(22, 138);
+            this.worker3.Name = "worker3";
+            this.worker3.Size = new System.Drawing.Size(64, 17);
+            this.worker3.TabIndex = 2;
+            this.worker3.Text = "worker3";
+            this.worker3.UseVisualStyleBackColor = true;
+            // 
+            // worker2
+            // 
+            this.worker2.AutoSize = true;
+            this.worker2.Location = new System.Drawing.Point(22, 92);
+            this.worker2.Name = "worker2";
+            this.worker2.Size = new System.Drawing.Size(64, 17);
+            this.worker2.TabIndex = 1;
+            this.worker2.Text = "worker2";
+            this.worker2.UseVisualStyleBackColor = true;
+            // 
+            // Chicken
+            // 
+            this.Chicken.AutoSize = true;
+            this.Chicken.Location = new System.Drawing.Point(22, 45);
+            this.Chicken.Name = "Chicken";
+            this.Chicken.Size = new System.Drawing.Size(64, 17);
+            this.Chicken.TabIndex = 0;
+            this.Chicken.Text = "worker1";
+            this.Chicken.UseVisualStyleBackColor = true;
+            // 
+            // btn_cnfrm
+            // 
+            this.btn_cnfrm.Location = new System.Drawing.Point(220, 456);
+            this.btn_cnfrm.Name = "btn_cnfrm";
+            this.btn_cnfrm.Size = new System.Drawing.Size(240, 23);
+            this.btn_cnfrm.TabIndex = 2;
+            this.btn_cnfrm.Text = "piu";
+            this.btn_cnfrm.UseVisualStyleBackColor = true;
+            this.btn_cnfrm.Click += new System.EventHandler(this.btn_cnfrm_Click);
+            // 
+            // txt_bx
+            // 
+            this.txt_bx.Location = new System.Drawing.Point(220, 385);
+            this.txt_bx.Multiline = true;
+            this.txt_bx.Name = "txt_bx";
+            this.txt_bx.Size = new System.Drawing.Size(240, 65);
+            this.txt_bx.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 517);
+            this.Controls.Add(this.txt_bx);
+            this.Controls.Add(this.btn_cnfrm);
+            this.Controls.Add(this.Check_Box);
+            this.Controls.Add(this.Radio_Box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Radio_Box.ResumeLayout(false);
+            this.Radio_Box.PerformLayout();
+            this.Check_Box.ResumeLayout(false);
+            this.Check_Box.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Radio_Box;
+        private System.Windows.Forms.GroupBox Check_Box;
+        private System.Windows.Forms.RadioButton NR3;
+        private System.Windows.Forms.RadioButton NR2;
+        private System.Windows.Forms.RadioButton NR1;
+        private System.Windows.Forms.CheckBox worker3;
+        private System.Windows.Forms.CheckBox worker2;
+        private System.Windows.Forms.CheckBox Chicken;
+        private System.Windows.Forms.Button btn_cnfrm;
+        private System.Windows.Forms.TextBox txt_bx;
+        private System.Windows.Forms.CheckBox worker4;
+        private System.Windows.Forms.CheckBox worker5;
+    }
+}
+
